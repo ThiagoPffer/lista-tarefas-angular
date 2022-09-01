46d237e0eed4ea31d1f0ebda5756991dabdb0ec6
Thiago Piffer <thiago.piffer@hotmail.com>
2022-09-01 17:20:26 -0300

    tarefa com horarios e checked

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Tarefa {
+    tarefa: String
+    horario: String
+    isRealizada: Boolean
+}
+
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
@@ -8,21 +14,31 @@ import { Component, OnInit } from '@angular/core';
 export class MainPageComponent implements OnInit {
 
     TAREFA_MIN_LENGTH: Number = 10;
-    tarefas: String[];
-    newTarefa: String;
+    EMPTY_TAREFA: Tarefa = {
+        tarefa: '',
+        horario: '',
+        isRealizada: false
+    }
+
+    tarefas: Tarefa[];
+    newTarefa: Tarefa;
 
     constructor() { 
         this.tarefas = [];
-        this.newTarefa = '';
+        this.newTarefa = { ...this.EMPTY_TAREFA };
     }
 
     ngOnInit(): void {}
 
     addTarefa(key?: String){
-        if ((!key || key === "Enter") && this.newTarefa && this.newTarefa !== '' && this.newTarefa.length > this.TAREFA_MIN_LENGTH ) {
+        if ((!key || key === "Enter") && this.newTarefa && this.newTarefa.tarefa !== '' && this.newTarefa.tarefa.length > this.TAREFA_MIN_LENGTH ) {
             this.tarefas.push(this.newTarefa);
-            this.newTarefa = '';
+            this.newTarefa = this.EMPTY_TAREFA;
         }
+    }
+
+    teste(item: Tarefa) {
+        console.log(item);
     }
 
 }
