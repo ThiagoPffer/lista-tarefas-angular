ee061eeaecb0b09a2c274459a076700efe650f09
Thiago Piffer <thiago.piffer@hotmail.com>
2022-09-01 15:30:14 -0300

    Initial files

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent implements OnInit {
+
+    TAREFA_MIN_LENGTH: Number = 10;
+    tarefas: String[] = [];
+    newTarefa: String = '';
+
+    constructor() { 
+        this.tarefas = ['Curso de angular com loiane', 'Almoço', 'Ler a bíblia e orar', 'Portfolio com Vue'];
+        this.newTarefa = '';
+    }
+
+    ngOnInit(): void {
+    }
+
+    addTarefa(key?: String){
+        if ((!key || key === "Enter") && this.newTarefa && this.newTarefa !== '' && this.newTarefa.length > this.TAREFA_MIN_LENGTH ) {
+            this.tarefas.push(this.newTarefa);
+            this.newTarefa = '';
+        }
+    }
+
+}
