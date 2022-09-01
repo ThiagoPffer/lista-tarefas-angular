ac7a17b54313f77b085320d80df3010aeb8d2cd8
Thiago Piffer <thiago.piffer@hotmail.com>
2022-09-01 15:36:12 -0300

    #mudar nome do projeto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/primeiro-projeto'),
+      dir: require('path').join(__dirname, './coverage/lista-tarefas-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'primeiro-projeto'`, () => {
+  it(`should have as title 'lista-tarefas-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('primeiro-projeto');
+    expect(app.title).toEqual('lista-tarefas-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('primeiro-projeto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lista-tarefas-angular app is running!');
   });
 });
